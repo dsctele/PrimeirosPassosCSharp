a139102a005c0c1e0b796bbe8521817a539543f9
Diego <dsctele@gmail.com>
2021-09-29 09:01:40 -0300

    primeiro upload pc particular

---
+++ CSharpPrimeirosPassos/8-CondicionaisDois/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _8_CondicionaisDois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 8 - Condicionais");
+
+            int idadeJoao = 18;
+            // int quantidadePessoas = 2;
+
+            // bool acompanhado = quantidadePessoas >= 2;
+
+            bool acompanhado = true
+
+            if (idadeJoao >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("João pode entrar");
+            }
+            else
+            {
+                Console.WriteLine("João não pode entrar!");
+            }
+       
+        }
+    }
+}
